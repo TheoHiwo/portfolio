2e1eaec3a31efbaabe1386be38c10002adb98af1
Theo <rara21400@gmail.com>
2024-01-30 18:24:28 -0500

    add tag icons

--- src/components/dom/Projects/TagIcons/TagIconCode.tsx
+++ src/components/dom/Projects/TagIcons/TagIconCode.tsx
@@ -1,34 +1,30 @@
-import {
-    SiReact,
-    SiTypescript,
-    SiJavascript,
-    SiAutohotkey,
-    SiChakraui,
-    SiTailwindcss,
-    SiGithub,
-  } from "react-icons/si";
-  import { TbBrandThreejs } from "react-icons/tb";
+import { SiReact, SiTypescript, SiJavascript, SiAutohotkey, SiChakraui, SiTailwindcss, SiGithub, SiUnity, SiBlender } from "react-icons/si";
+import { TbBrandThreejs } from "react-icons/tb";
 
 import { Tags } from "../data";
 
 export default function TagIcon({ tag, size }: { tag: Tags; size: number }) {
-    return (
-      <div className="pt-1">
-        {tag === "ReactJS" ? (
+  return (
+    <div className="pt-1">
+      {tag === "ReactJS" ? (
         <SiReact size={`${size}em`} />
-        ) : tag === "TypeScript" ? (
+      ) : tag === "TypeScript" ? (
         <SiTypescript size={`${size}em`} />
-        ) : tag === "Javascript" ? (
+      ) : tag === "Javascript" ? (
         <SiJavascript size={`${size}em`} />
-        ) : tag === "AutoHotKey" ? (
+      ) : tag === "AutoHotKey" ? (
         <SiAutohotkey size={`${size}em`} />
-        ) : tag === "ChakraUI" ? (
+      ) : tag === "Unity" ? (
+        <SiUnity size={`${size}em`} />
+      ) : tag === "Blender" ? (
+        <SiBlender size={`${size}em`} />
+      ) : tag === "ChakraUI" ? (
         <SiChakraui size={`${size}em`} />
-        ) : tag === "TailwindCSS" ? (
+      ) : tag === "TailwindCSS" ? (
         <SiTailwindcss size={`${size}em`} />
-        ) : tag === "ThreeJS" ? (
-        <TbBrandThreejs size={`${size}em`} />) : tag === "BabylonJS" ? null : tag === "P5JS"
-        ? null : null}
-      </div>
-    );
-  }
+      ) : tag === "ThreeJS" ? (
+        <TbBrandThreejs size={`${size}em`} />
+      ) : tag === "BabylonJS" ? null : tag === "P5JS" ? null : null}
+    </div>
+  );
+}
